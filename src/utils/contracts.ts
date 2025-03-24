240011cca6f8281c030e5dbbd6874519925ab985
Juan <juandahl@users.noreply.github.com>
2025-03-23 21:31:05 -0300

    feat: add modal with integration (#11)

---
+++ src/utils/contracts.ts
@@ -0,0 +1,14 @@
+import { baseSepolia } from "wagmi/chains";
+
+export const defaultChainId = baseSepolia.id as number;
+
+export const contracts= {
+  [baseSepolia.id as number]: {
+    // TODO: move to db
+    kritties: "0x42575Ac7e60d9927C426b724e750621a52E067F1",    
+    paymentToken: {
+      address: "0x16760858e6D70D2C1a6657517437Bd0DB4D85171",
+      decimals: 18,
+    },
+  },
+} as const;
